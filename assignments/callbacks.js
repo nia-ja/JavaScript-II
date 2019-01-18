d0e0cf9b88a028a759458e9a8aeb467cc7ca2009
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-01-17 21:13:37 -0800

    Added solution for multiplyNums()

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -47,17 +47,30 @@ function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
   return cb(x+y);
 }
-sumNums(5, 6, function (addition) {
-  console.log(addition);
+sumNums(5, 6, function (add) {
+  console.log(add);
 });
 
+
+
+
+    // Multiplies two numbers
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
+multiplyNums(5, 6, function (multiply) {
+  console.log(multiply);
+});
+
+
+
+
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  
 }
 
 /* STRETCH PROBLEM */
